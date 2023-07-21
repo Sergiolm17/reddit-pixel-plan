a2f4bb13b0573051f09bbae255119b5e8cb72e8c
Sergio Lazaro Mondargo <sergiolazaromondargo@gmail.com>
2023-07-21 01:14:27 -0500

    twitter href

--- src/components/Twitter.tsx
+++ src/components/Twitter.tsx
@@ -1,18 +1,21 @@
 
 export default function Twitter() {
     return (
-        <p
+        <a
+            href="https://twitter.com/srsergiolazaro"
+            target="_blank"
             style={{
                 position: "fixed",
                 bottom: "7px",
                 right: "7px",
                 color: "gray",
                 fontSize: "1rem",
+                textDecoration: "none",
                 fontWeight: "bold",
             }}
         >
             @srsergiolazaro
 
-        </p>
+        </a>
     )
 }
